672210b3646e659da6799ea3f4de8142af807c0e
connorm400 <125999884+connorm400@users.noreply.github.com>
2023-07-31 22:28:10 -0600

    changed a couple of things

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
 
 const IP: &str = "127.0.0.1";
 const PORT: u16 = 7878;
-const PATH: &str = "www";
+const DOCUMENTS_PATH: &str = "www";
 
 fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind(format!("{IP}:{PORT}").as_str())?;
@@ -23,9 +23,12 @@ fn main() -> Result<(), std::io::Error> {
 fn handle_client(mut stream: TcpStream) {
     let reader = BufReader::new(&mut stream);
     let request_line = reader.lines()
-        .next()
-        .unwrap().unwrap();
+        .next() // move over the first line 
+        .unwrap().unwrap(); //unwraps the Option and the Result
     
+
+    // we need the HTTP method and the path from the tcp stream. I split it with the space. 
+    // I dont think its possible for the unwraps to panic since the two elements should always be there
     let mut split_request_iter = request_line.split(" ");
     let method = split_request_iter.next().unwrap();
     let path = split_request_iter.next().unwrap();
@@ -39,20 +42,20 @@ fn handle_client(mut stream: TcpStream) {
 
     let mut file_to_read = String::new();
     
-    if path == "/" {file_to_read.push_str(format!("{PATH}/index.html").as_str())}
+    if path == "/" {file_to_read.push_str(format!("{DOCUMENTS_PATH}/index.html").as_str())}
     // put all custom file formats here
-    else if path.contains(".css") {file_to_read.push_str(format!("{PATH}/{path}").as_str())} 
-    //else if path.contains(".png") {file_to_read.push_str(format!("{PATH}/{path}").as_str())} 
-    //else if path.contains(".ico") {file_to_read.push_str(format!("{PATH}/{path}").as_str())}
+    else if path.contains(".css") {file_to_read.push_str(format!("{DOCUMENTS_PATH}/{path}").as_str())} 
+    //else if path.contains(".png") {file_to_read.push_str(format!("{DOCUMENTS_PATH}/{path}").as_str())} 
+    //else if path.contains(".ico") {file_to_read.push_str(format!("{DOCUMENTS_PATH}/{path}").as_str())}
     
-    else {file_to_read.push_str(format!("{PATH}/{path}.html").as_str())}
+    else {file_to_read.push_str(format!("{DOCUMENTS_PATH}/{path}.html").as_str())}
 
     let (status, contents) = match fs::read_to_string(file_to_read) {
         Ok(c) => ("HTTP/1.1 200 OK", c),
         Err(_e) => { // we'll just assume that the error is just that the file doesnt exist
             //println!("{:?}", _e);
-            ("HTTP/1.1 404 NOT FOUND", fs::read_to_string(format!("{PATH}/404.html").as_str())
-                .expect(format!("must have 404 in path ./{PATH}/404.html").as_str()))
+            ("HTTP/1.1 404 NOT FOUND", fs::read_to_string(format!("{DOCUMENTS_PATH}/404.html").as_str())
+                .expect(format!("must have 404 in path ./{DOCUMENTS_PATH}/404.html").as_str()))
         }
     };
 
